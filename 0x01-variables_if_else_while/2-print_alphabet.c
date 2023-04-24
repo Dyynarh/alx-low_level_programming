c6a9c35bc19b8257cf321b53fc1f68060ccceeb8
dyynarh <dyynarh@gmail.com>
2023-04-24 13:53:22 -0700

    Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-
 /**
-  *main - print the alphabet
-  *Description: A program that prints alphabet in lower case
-  *Return: Always 0
+  *main - Entry point
+  *
+  *Return: Always 0 (success)
   */
+
 int main(void)
 {
-	char letter = 'a';
-	char alphabet(26); = "abcdefghijklmnopqrstuvwxyz"
-	int i;
+	char c;
 
-	for (i = 0, i < 26, i++)
-	{
-		putchar[alphabet[i]i;
-	putchar ('\n)
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
 	return (0);
 }

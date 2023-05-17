fb1ec305ba58d9714c3321152146ec861ca01b73
dyynarh <dyynarh@gmail.com>
2023-05-17 11:51:55 -0700

    A function that returns a ponter

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,56 +1,43 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * alloc_grid - ...
- * @width: ...
- * @height: ...
- *
- * Return: ...
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
  */
-
 int **alloc_grid(int width, int height)
 {
-	int i, j, k, l;
-	int **a;
+	int **aee;
+	int k, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	a = malloc(sizeof(int *) * height);
+	aee = malloc(sizeof(int *) * height);
 
-	if (a == NULL)
-	{
-		free(a);
+	if (aee == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < height; i++)
+	for (k = 0; k < height; k++)
 	{
-		a[i] = malloc(sizeof(int) * width);
+		aee[k] = malloc(sizeof(int) * width);
 
-		if (a[i] == NULL)
+		if (aee[k] == NULL)
 		{
+			for (; k  >= 0; k--)
+				free(aee[k]);
 
-			for (j = i; j >= 0; j--)
-			{
-				free(a[j]);
-			}
-
-			free(a);
-		}	return (NULL);
-
+			free(aee);
+			return (NULL);
+		}
 	}
 
 	for (k = 0; k < height; k++)
 	{
-		for (l = 0; l < width; l++)
-		{
-		a[k][l] = 0;
-		}
-
+		for (j = 0; j < width; j++)
+			aee[k][j] = 0;
 	}
 
-	return (a); 
+	return (aee);
 }
